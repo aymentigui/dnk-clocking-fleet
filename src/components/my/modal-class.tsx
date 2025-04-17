9616f73eb6c685b8f406c1c2ff754239ea3c02d7
Aymen tigui <aymentigui@gmail.com>
2025-04-17 18:07:33 +0100

    responsive design 2

--- src/components/my/modal-class.tsx
+++ src/components/my/modal-class.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const ModalClass = () => {
   return (
     <div className='hidden'>
-        <div className='w-[70%] max-w-[70%] h-[80%] lg:grid-cols-2'></div>
+        <div className='w-[70%] max-w-[70%] h-[80%] lg:grid-cols-2 overflow-auto'></div>
+        <div className='h-[50%] lg:h-[70%] flex flex-col'></div>
+        <div className='h-0 lg:h-10'></div>
     </div>
   )
 }
--- src/modals/add-update-dialog-vehicle.tsx
+++ src/modals/add-update-dialog-vehicle.tsx
@@ -34,7 +34,7 @@ export const AddUpdateDialogVehicle = () => {
   const [parks, setParks] = useState<any[]>([{ name: "----", id: "" }]);
   const [hasPermissionAffectation, setHasPermissionAffectation] = useState(false);
   const origin = useOrigin()
-  const {session} = useSession()
+  const { session } = useSession()
 
 
 
@@ -67,7 +67,7 @@ export const AddUpdateDialogVehicle = () => {
   useEffect(() => {
     getParksAdmin().then((res) => {
       if (res && res.status === 200) {
-        setParks([...parks,...res.data])
+        setParks([...parks, ...res.data])
       }
     });
   }, []);
@@ -76,7 +76,7 @@ export const AddUpdateDialogVehicle = () => {
     if (vehicle) {
       form.setValue("matricule", vehicle.matricule ?? "");
       form.setValue("model", vehicle.model ?? "");
-      form.setValue("year", vehicle.year?String(vehicle.year):"");
+      form.setValue("year", vehicle.year ? String(vehicle.year) : "");
       form.setValue("brand", vehicle.brand ?? "");
       form.setValue("vin", vehicle.vin ?? "");
       form.setValue("park", vehicle.parkId);
@@ -85,9 +85,9 @@ export const AddUpdateDialogVehicle = () => {
 
   useEffect(() => {
     if (session) {
-      setHasPermissionAffectation(isAdd?
+      setHasPermissionAffectation(isAdd ?
         (session?.user?.permissions.find((permission: string) => permission === "vehicles_park_create") ?? false) || session?.user?.is_admin
-        :(session?.user?.permissions.find((permission: string) => permission === "vehicles_park_update") ?? false) || session?.user?.is_admin
+        : (session?.user?.permissions.find((permission: string) => permission === "vehicles_park_update") ?? false) || session?.user?.is_admin
       )
     }
   }, [session])
@@ -140,137 +140,139 @@ export const AddUpdateDialogVehicle = () => {
 
   return (
     <Dialog open={isOpen} onOpenChange={handleClose}>
-      <DialogContent className="w-[70%] max-w-[70%] gb-r h-[50%] overflow-y-auto">
+      <DialogContent className="w-[70%] max-w-[70%] flex flex-col h-[50%] lg:h-[70%]">
         <DialogHeader>
           <DialogTitle className="text-center">{isAdd ? v("addvehicle") : v("updatevehicle")}</DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-              {/* matricule */}
-              <FormField
-                control={form.control}
-                name="matricule"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{v("matricule")}</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder={v("matricule")}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* model */}
-              <FormField
-                control={form.control}
-                name="model"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{v("model")}</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder={v("model")}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 overflow-auto h-fit">
+            <div className="px-2">
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                {/* matricule */}
+                <FormField
+                  control={form.control}
+                  name="matricule"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{v("matricule")}</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder={v("matricule")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* model */}
+                <FormField
+                  control={form.control}
+                  name="model"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{v("model")}</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder={v("model")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-              {/* Name */}
-              <FormField
-                control={form.control}
-                name="year"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{v("year")}</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        {...field}
-                        placeholder={v("year")}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* model */}
-              <FormField
-                control={form.control}
-                name="brand"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{v("brand")}</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder={v("brand")}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                {/* Name */}
+                <FormField
+                  control={form.control}
+                  name="year"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{v("year")}</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          {...field}
+                          placeholder={v("year")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* model */}
+                <FormField
+                  control={form.control}
+                  name="brand"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{v("brand")}</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder={v("brand")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-            <div className="grid grid-cols-1 gap-4">
-              {/* Name */}
-              <FormField
-                control={form.control}
-                name="vin"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{v("vin")}</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder={v("vin")}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-             { hasPermissionAffectation && <FormField
-                control={form.control}
-                name="park"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{v("park")}</FormLabel>
-                    <FormControl>
-                      <Select
-                        options={
-                          parks?.map((p) => ({
-                            value: p.id,
-                            label: p.name,
-                          }))
-                        }
-                        value={
-                          {
-                            value: field.value,
-                            label: parks?.find((p) => p.id === field.value)?.name,
+              <div className="grid grid-cols-1 gap-4">
+                {/* Name */}
+                <FormField
+                  control={form.control}
+                  name="vin"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{v("vin")}</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder={v("vin")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {hasPermissionAffectation && <FormField
+                  control={form.control}
+                  name="park"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{v("park")}</FormLabel>
+                      <FormControl>
+                        <Select
+                          options={
+                            parks?.map((p) => ({
+                              value: p.id,
+                              label: p.name,
+                            }))
                           }
-                        }
-                        onChange={(selectedOptions) => {
-                          field.onChange(
-                            selectedOptions ? selectedOptions.value : ""
-                          );
-                        }}
-                        placeholder={v("selectpark")}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />}
+                          value={
+                            {
+                              value: field.value,
+                              label: parks?.find((p) => p.id === field.value)?.name,
+                            }
+                          }
+                          onChange={(selectedOptions) => {
+                            field.onChange(
+                              selectedOptions ? selectedOptions.value : ""
+                            );
+                          }}
+                          placeholder={v("selectpark")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />}
+              </div>
             </div>
 
             {/* Submit Button */}
@@ -279,7 +281,7 @@ export const AddUpdateDialogVehicle = () => {
               {isAdd ? v("addvehicle") : v("updatevehicle")}
             </Button>
           </form>
-          <div className="w-full h-20"></div>
+          {/* <div className="w-full h-0 lg:h-10"></div> */}
         </Form>
       </DialogContent>
     </Dialog>
