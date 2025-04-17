4a8f0ece2ac903f31f9df0d0b0d9ab9b530a7b2b
Aymen tigui <aymentigui@gmail.com>
2025-04-17 11:23:05 +0100

    login api

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,13 +1,5 @@
 import { loginUser } from "@/actions/auth/auth";
-import { verifySession } from "@/actions/permissions";
 import { NextResponse, NextRequest } from "next/server";
-
-const headersGet = {
-    "Access-Control-Allow-Origin": "http://localhost:3001",
-    "Access-Control-Allow-Methods": "GET",
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'
-}
 
 const headersPost = {
     "Access-Control-Allow-Origin": "http://localhost:3001",
@@ -16,14 +8,14 @@ const headersPost = {
     'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'
 }
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
 
     const token = await loginUser({ email: "admin@admin.com", password: "test123" });
     // console.log(token)
 
     if (token.status === 200) {
         const response = NextResponse.json({ message: "login success", userId: token.data.id, }, {
-            headers:headersGet
+            headers:headersPost
         });
         return response;
     } else {
@@ -36,28 +28,11 @@ export async function GET(request: NextRequest) {
 }
 
 
-export async function POST(request: NextRequest) {
-    console.log(1)
-
-    const verify = await verifySession()
-    console.log("verify", verify)
-
-    const body = await request.json();
-    const { name, description } = body;
-    console.log("name", name);
-
-    return NextResponse.json({ message: "test2" }, {
-        headers:headersPost
-    });
-}
-
-
-
 export async function OPTIONS() {
     const response = NextResponse.json({ message: 'CORS preflight successful!' });
     // Add CORS headers for preflight request
     response.headers.set('Access-Control-Allow-Origin', "http://localhost:3001"); // Allow the frontend origin
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow GET, POST, OPTIONS methods
+    response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS'); // Allow GET, POST, OPTIONS methods
     response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'); // Allow specific headers
     response.headers.set('Access-Control-Allow-Credentials', 'true'); // Allow credentials if needed
     return response;
