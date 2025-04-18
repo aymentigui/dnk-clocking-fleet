5fe651ff4ffbdc299f0370e40ca6edf4fae5db96
Aymen tigui <aymentigui@gmail.com>
2025-04-18 13:06:19 +0100

    api cors clocking

--- src/app/api/admin/clocking/route.ts
+++ src/app/api/admin/clocking/route.ts
@@ -1,13 +1,11 @@
 import { createClocking } from "@/actions/clocking/set";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function OPTIONS(request: Request) {
-    const response = NextResponse.json({ message: 'CORS preflight successful!' });
-    // Add CORS headers for preflight request
-    response.headers.set('Access-Control-Allow-Origin', process.env.API_CORS_AUTORIZED??"http://localhost:3001"); // Allow the frontend origin
-    response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization');
-    return response;
+const headersPost = {
+    "Access-Control-Allow-Origin": process.env.API_CORS_AUTORIZED??"http://localhost:3001",
+    "Access-Control-Allow-Methods": "POST",
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'
 }
 
 export async function POST(request: Request) {
@@ -20,12 +18,17 @@ export async function POST(request: Request) {
         { data: res },
         {
             status: 200,
-            headers: {
-                'Access-Control-Allow-Origin': process.env.API_CORS_AUTORIZED??"http://localhost:3001",
-                'Access-Control-Allow-Methods': 'POST, OPTIONS',
-                'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization',
-                'Content-Type': 'application/json',
-            },
+            headers: headersPost
         }
     );
+}
+
+export async function OPTIONS() {
+    const response = NextResponse.json({ message: 'CORS preflight successful!' });
+    // Add CORS headers for preflight request
+    response.headers.set('Access-Control-Allow-Origin', process.env.API_CORS_AUTORIZED??"http://localhost:3001"); // Allow the frontend origin
+    response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS'); // Allow GET, POST, OPTIONS methods
+    response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'); // Allow specific headers
+    response.headers.set('Access-Control-Allow-Credentials', 'true'); // Allow credentials if needed
+    return response;
 }
