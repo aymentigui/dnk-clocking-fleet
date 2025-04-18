842f5b5984adab074077df51204044faca7d4569
Aymen tigui <aymentigui@gmail.com>
2025-04-18 17:13:00 +0100

    auth.ts cookies

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/db";
 import { NextResponse, NextRequest } from "next/server";
 
 const headersPost = {
-    "Access-Control-Allow-Origin": process.env.API_CORS_AUTORIZED??"http://localhost:3001",
+    "Access-Control-Allow-Origin": process.env.API_CORS_AUTORIZED ?? "http://localhost:3001",
     "Access-Control-Allow-Methods": "POST",
     'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'
@@ -13,8 +13,12 @@ export async function POST(request: NextRequest) {
 
     const data = await request.json();
     const { email, password } = data;
+    const cookies = request.cookies;
+    const deviceType = cookies.get('type')?.value || 'unknown';
+    console.log(deviceType)
 
-    const token = await loginUser({ email, password});
+
+    const token = await loginUser({ email, password });
     // console.log(token)
 
     if (!token || token.status !== 200) {
@@ -47,7 +51,7 @@ export async function POST(request: NextRequest) {
 export async function OPTIONS() {
     const response = NextResponse.json({ message: 'CORS preflight successful!' });
     // Add CORS headers for preflight request
-    response.headers.set('Access-Control-Allow-Origin', process.env.API_CORS_AUTORIZED??"http://localhost:3001"); // Allow the frontend origin
+    response.headers.set('Access-Control-Allow-Origin', process.env.API_CORS_AUTORIZED ?? "http://localhost:3001"); // Allow the frontend origin
     response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS'); // Allow GET, POST, OPTIONS methods
     response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'); // Allow specific headers
     response.headers.set('Access-Control-Allow-Credentials', 'true'); // Allow credentials if needed
