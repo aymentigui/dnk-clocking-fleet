37c412e0304996d88442fc73a2906acd1317e498
Aymen tigui <aymentigui@gmail.com>
2025-04-19 13:51:21 +0100

    test api set clocking change

--- src/app/api/admin/clocking/route.ts
+++ src/app/api/admin/clocking/route.ts
@@ -12,7 +12,8 @@ const headersPost = {
 
 export async function POST(request: Request) {
     const { matricule, type } = await request.json();
-    console.log(request.json)
+    const data= await request.json()
+    console.log(data)
     const authHeader = request.headers.get("Authorization");
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return NextResponse.json({ error: "Token manquant" }, {
