227c87eb607357c41033ead046010a5ef64df6d7
Aymen tigui <aymentigui@gmail.com>
2025-04-20 11:03:33 +0100

    add clockings page2

--- src/app/admin/clockings/_component/list-clockings.tsx
+++ src/app/admin/clockings/_component/list-clockings.tsx
@@ -4,8 +4,7 @@ import { DataTable } from "./data-table/data-table-clocking";
 
 
 export default function ClockingList() {
-
-
+  
   return (
     <div className="container mx-auto pb-10">
       <DataTable
