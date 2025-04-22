262a5a77e43e0bd632b4b4f3fb91104f144699f2
Aymen tigui <aymentigui@gmail.com>
2025-04-22 09:24:22 +0100

    region

--- src/actions/clocking/set.ts
+++ src/actions/clocking/set.ts
@@ -15,6 +15,8 @@ export async function createClocking(data: any) {
             return { status: 401, data: { message: e('unauthorized') } }
         }
 
+        console.log(session)
+
         const existingDevice = await prisma.device.findFirst({
             where: { user_id: session.data.user.id },
         });
