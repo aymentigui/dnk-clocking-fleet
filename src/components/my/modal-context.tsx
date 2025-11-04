bf4ae6c7624b924334ae1b8683c1e60e9f27025e
Aymen tigui <aymentigui@gmail.com>
2025-11-05 00:26:08 +0100

    maj

--- src/components/my/modal-context.tsx
+++ src/components/my/modal-context.tsx
@@ -1,11 +1,7 @@
 import { AddUpdateUserDialogProvider } from '@/context/add-update-dialog-context'
-import { AddUpdateDeviceDialogProvider } from '@/context/add-update-dialog-context-device'
-import { AddUpdateParkDialogProvider } from '@/context/add-update-dialog-context-park'
 import { AddUpdateRegionDialogProvider } from '@/context/add-update-dialog-context-region'
 import { AddUpdateVehicleDialogProvider } from '@/context/add-update-dialog-context-vehicle'
 import { AddUpdateUserDialog } from '@/modals/add-update-dialog'
-import { AddUpdateDeviceDialog } from '@/modals/add-update-dialog-device'
-import { AddUpdateDialogPark } from '@/modals/add-update-dialog-park'
 import { AddUpdateDialogRegion } from '@/modals/add-update-dialog-region'
 import { AddUpdateDialogVehicle } from '@/modals/add-update-dialog-vehicle'
 import React from 'react'
@@ -14,20 +10,14 @@ const ModalContext = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
             <AddUpdateUserDialogProvider>
-                <AddUpdateParkDialogProvider>
                 <AddUpdateRegionDialogProvider>
                     <AddUpdateVehicleDialogProvider>
-                        <AddUpdateDeviceDialogProvider>
-                            {children}
-                            <AddUpdateUserDialog />
-                            <AddUpdateDialogPark />
-                            <AddUpdateDialogRegion />
-                            <AddUpdateDialogVehicle />
-                            <AddUpdateDeviceDialog />
-                        </AddUpdateDeviceDialogProvider>
+                        {children}
+                        <AddUpdateUserDialog />
+                        <AddUpdateDialogRegion />
+                        <AddUpdateDialogVehicle />
                     </AddUpdateVehicleDialogProvider>
-                    </AddUpdateRegionDialogProvider>
-                </AddUpdateParkDialogProvider>
+                </AddUpdateRegionDialogProvider>
             </AddUpdateUserDialogProvider>
         </>
     )
