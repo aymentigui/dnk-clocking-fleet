9495162e00260250090c0cf7641259d34c2fdd7a
Aymen tigui <aymentigui@gmail.com>
2025-11-04 18:57:40 +0100

    maj

--- src/app/admin/clockings/_component/ClockingsTable.tsx
+++ src/app/admin/clockings/_component/ClockingsTable.tsx
@@ -128,7 +128,7 @@ export function ClockingsTable({ clockings, loading }: ClockingsTableProps) {
                                 </span>
                             </td>
                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
-                                {clocking.conducteur?.firstname + clocking.conducteur?.lastname + clocking.conducteur?.matricule || "-"}
+                                {clocking.conducteur?.firstname + " " + clocking.conducteur?.lastname + " " + clocking.conducteur?.matricule || "-"}
                             </td>
                             <td className="px-6 py-4 whitespace-nowrap text-sm">
                                 <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${getStatusColor(clocking.status)}`}>
