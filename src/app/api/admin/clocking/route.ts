d7e3640c71f6a60e40fc42f611c6e8188f6fb9bc
Aymen tigui <aymentigui@gmail.com>
2025-11-04 18:32:07 +0100

    test cors

--- src/app/api/admin/clocking/route.ts
+++ src/app/api/admin/clocking/route.ts
@@ -2,27 +2,9 @@ import { createClocking } from "@/actions/clocking/set";
 import { withAuth } from "@/actions/util/with-auth";
 import { NextResponse } from "next/server";
 
-// const headersPost = {
-//     "Access-Control-Allow-Origin": process.env.API_CORS_AUTORIZED ?? "http://localhost:3001",
-//     "Access-Control-Allow-Methods": "POST",
-//     'Access-Control-Allow-Credentials': 'true',
-//     'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'
-// }
 
 export const POST= withAuth(async (request, { user }) => {
     const data = await request.json();
-    // console.log("Received clocking data:", data);
     const res = await createClocking({ matricule: data.matricule, type: Number(data.type??0), conducteur_id: data.conducteur_id });
-    // console.log("Clocking creation response:", res);
     return NextResponse.json({ data: res.data }, { status: res.status });
 });
-
-// export async function OPTIONS() {
-//     const response = NextResponse.json({ message: 'CORS preflight successful!' });
-//     // Add CORS headers for preflight request
-//     response.headers.set('Access-Control-Allow-Origin', process.env.API_CORS_AUTORIZED ?? "http://localhost:3001"); // Allow the frontend origin
-//     response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS'); // Allow GET, POST, OPTIONS methods
-//     response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'); // Allow specific headers
-//     response.headers.set('Access-Control-Allow-Credentials', 'true'); // Allow credentials if needed
-//     return response;
-// }
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,25 +3,15 @@ import { encrypt } from "@/actions/util/util";
 import { prisma } from "@/lib/db";
 import { NextResponse, NextRequest } from "next/server";
 
-const headersPost = {
-    "Access-Control-Allow-Origin": process.env.API_CORS_AUTORIZED??"http://localhost:3001",
-    "Access-Control-Allow-Methods": "POST",
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'
-}
-
 export async function POST(request: NextRequest) {
 
     const data = await request.json();
     const { email, password } = data;
 
     const token = await loginUser({ email, password});
-    // console.log(token)
-
     if (!token || token.status !== 200) {
         return NextResponse.json({ message: "login failed" }, {
             status: 400,
-            headers: headersPost
         }
         );
     }
@@ -30,31 +20,15 @@ export async function POST(request: NextRequest) {
         where: { user_id: token.data.id },
     });
 
-    console.log(token)
-
     if (!device) {
         return NextResponse.json({ message: "login failed" }, {
             status: 400,
-            headers: headersPost
         });
     }
 
     const encryptData = encrypt(token);
 
-    const response = NextResponse.json({ message: "login success", token: encryptData, type: device.type }, {
-        headers: headersPost
-    });
+    const response = NextResponse.json({ message: "login success", token: encryptData, type: device.type });
     return response;
 
-}
-
-
-export async function OPTIONS() {
-    const response = NextResponse.json({ message: 'CORS preflight successful!' });
-    // Add CORS headers for preflight request
-    response.headers.set('Access-Control-Allow-Origin', process.env.API_CORS_AUTORIZED??"http://localhost:3001"); // Allow the frontend origin
-    response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS'); // Allow GET, POST, OPTIONS methods
-    response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'); // Allow specific headers
-    response.headers.set('Access-Control-Allow-Credentials', 'true'); // Allow credentials if needed
-    return response;
 }
